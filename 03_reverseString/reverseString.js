90191f063281a23758581d79ab2a2302de0f387f
Devin Bedford <bedforddevin895@gmail.com>
2023-05-16 18:51:55 +0200

    Finish exercise.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let letters = [];
+    for (let i = word.length; i >= 0; i--){
+        letters.push(word.charAt(i));
+    }
+    return letters.join("");
 };
 
 // Do not edit below this line
