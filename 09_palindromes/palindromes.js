2ada5cf13c3a62aa175aee0e4dffa667ca5bde2e
Devin Bedford <bedforddevin895@gmail.com>
2023-05-16 22:05:14 +0200

    Test 1 pass.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let letters_backwards = [];
+    let letters = []
+
+    for (let i = string.length-1; i >= 0 ; i--){
+        if(string.charAt(i).toUpperCase() != string.charAt(i).toLowerCase()) {
+            letters_backwards.push(string.charAt(i));
+        };
+    };
+    for (let i = 0; i <= string.length-1 ; i++){
+        if(string.charAt(i).toUpperCase() != string.charAt(i).toLowerCase()) {
+            letters.push(string.charAt(i));
+        };
+    };
+    if (letters_backwards.join("") === letters.join("")){
+        return true;
+    }
+    else{
+        return false;
+    }
 
 };
 
