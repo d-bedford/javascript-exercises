0a4de0e0c7923856d664f016a3ee2975e34fbb66
Devin Bedford <bedforddevin895@gmail.com>
2023-05-16 19:45:12 +0200

    Finish exercise.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...args) {
+    
+    args.forEach((arg) =>{
+        let index = inputArray.indexOf(arg);
+        if (index>-1){
+            inputArray.splice(index, 1);
+        }
+    });
+    
+    return inputArray;
 };
 
 // Do not edit below this line
