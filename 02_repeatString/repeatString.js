a408828379757d5bb8da5a4b67424618c183cf8d
Devin Bedford <bedforddevin895@gmail.com>
2023-05-16 18:39:40 +0200

    Finish exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, num_times) {
+    let words = [];
+    for(let i=0; i<num_times; i++){
+      words[i] = word;
+    }
+    if (num_times < 0){
+        return "ERROR";
+    }
+    return words.join("");
 };
 
 // Do not edit below this line
