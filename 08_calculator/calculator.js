29bde2bdebfb08a62e2552a679c8912feafeb866
Devin Bedford <bedforddevin895@gmail.com>
2023-05-16 21:37:41 +0200

    Partially finish.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let tot = 0;
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	let tot = 0;
+  for (const num of nums){
+    tot+=num;
+  }
+  return tot;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+	let tot = 0;
+  for (let i = 0; i<nums.length-1; i++){
+    tot += nums[i] * nums[i+1];
+  }
+  return tot;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0){
+    return 1;
+  }
+	let fact = 1;
+  for (i = 1; i <= num; i++) {
+      fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
