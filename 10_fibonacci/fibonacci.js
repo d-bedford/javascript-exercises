4ceca11ed203ec3232e15c9b488b6a4493f8535d
Devin Bedford <bedforddevin895@gmail.com>
2023-05-17 18:59:16 +0200

    Finish exercise.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let number = parseInt(num);
+    if (number < 0){
+        return "OOPS";
+    };
+    let sequence = [1, 1];
+    while(sequence.length < number){
+        sequence.push(sequence[sequence.length-1] + sequence[sequence.length-2]);
+    };
+    return sequence[number-1];
 };
 
 // Do not edit below this line
