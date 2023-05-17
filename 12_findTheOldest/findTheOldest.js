e9e8a304b6c66623d270bacd4b8f843f660e2d16
Devin Bedford <bedforddevin895@gmail.com>
2023-05-17 20:57:34 +0200

    Finish exercise.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = 0;
+    const date = new Date();
+    for (let person of people){
+        let age = person["yearOfDeath"] - person["yearOfBirth"];
+        if (oldest === 0){
+            if (!("yearOfDeath" in person)){
+                let curr_age = date.getFullYear() - person["yearOfBirth"];
+                person["age"] = curr_age;
+            }
+            oldest = person;
+        }
+        else if ((oldest["yearOfDeath"] - oldest["yearOfBirth"]) < age || oldest["age"] < age){
+            oldest = person;
+        };
+    }
+    return oldest;
 };
 
 // Do not edit below this line
